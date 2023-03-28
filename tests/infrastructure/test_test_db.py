fa06c3202db73d77ec901312cd4d7b56332f5a70
Rob Knop <raknop@lbl.gov>
2023-03-28 13:56:10 -0700

    Docker images for postgres and application.  Refactor run-tests.yml to use the images.

--- tests/infrastructure/test_test_db.py
+++ tests/infrastructure/test_test_db.py
@@ -5,44 +5,42 @@ import psycopg2
 import psycopg2.extras
 
 @pytest.fixture(scope='module')
-def db():
-    host = 'localhost'
-    port = 5433
+def db_con_and_cursor():
+    host = 'seechange-postgres'
+    port = 5432
     user = 'postgres'
     password = 'fragile'
     dbname = 'seechange'
 
     con = psycopg2.connect( dbname=dbname, host=host, port=port, user=user, password=password )
-    yield con
+    cursor = con.cursor( cursor_factory=psycopg2.extras.RealDictCursor )
+    yield con, cursor
+    con.rollback()
     con.close()
 
 @pytest.fixture(scope='module')
-def cursor( db ):
-    cursor = db.cursor( cursor_factory=psycopg2.extras.RealDictCursor )
-    yield cursor
-    cursor.rollback()
-    cursor.close()
-    
-@pytest.fixture(scope='module')
-def testtable( cursor ):
+def testtable( db_con_and_cursor ):
+    con, cursor = db_con_and_cursor
     cursor.execute( "CREATE TABLE testing ( string text, value int primary key )" )
-    cursor.commit()
+    con.commit()
     yield True
     cursor.execute( "DROP TABLE testing" )
-    cursor.commit()
+    con.commit()
 
 @pytest.fixture(scope='module')
-def insertrows( cursor, testtable ):
+def insertrows( db_con_and_cursor, testtable ):
+    con, cursor = db_con_and_cursor
     cursor.execute( "INSERT INTO testing(string,value) VALUES ('one',1)" )
     cursor.execute( "INSERT INTO testing(string,value) VALUES ('two',2)" )
-    cursor.commit()
+    con.commit()
     yield True
     cursor.execute( "DELETE FROM testing WHERE value IN (1,2)" )
-    cursor.commit()
+    con.commit()
                         
-def test_database( cursor, insertrows ):
+def test_database( db_con_and_cursor, insertrows ):
+    con, cursor = db_con_and_cursor
     cursor.execute( "SELECT * FROM testing ORDER BY value" )
     rows = cursor.fetchall()
     assert len(rows) == 2
-    assert rows[0]['text'] == 'one' and rows[0]['value'] == 1
-    assert rows[1]['text'] == 'two' and rows[1]['value'] == 2
+    assert rows[0]['string'] == 'one' and rows[0]['value'] == 1
+    assert rows[1]['string'] == 'two' and rows[1]['value'] == 2
