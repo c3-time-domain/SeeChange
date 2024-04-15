3b7321c29ec01b81ad6f0c9dfe95c8726efacee9
whohensee <106775295+whohensee@users.noreply.github.com>
2024-04-15 11:20:38 -0700

    Improve line spacing

--- tests/improc/test_alignment.py
+++ tests/improc/test_alignment.py
@@ -26,10 +26,9 @@ def test_warp_decam( decam_datastore, decam_reference ):
         warped.filepath = f'warp_test_{"".join(random.choices("abcdefghijklmnopqrstuvwxyz",k=10))}'
 
         assert warped.data.shape == ds.image.data.shape
+
         oob_bitflag = string_to_bitflag( 'out of bounds', flag_image_bits_inverse)
         badpixel_bitflag = string_to_bitflag( 'bad pixel', flag_image_bits_inverse)
-
-
         assert (warped.flags == oob_bitflag).sum() > (warped.flags == badpixel_bitflag).sum()
 
         # Check a couple of spots on the image
