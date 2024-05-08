eb96b0f8d55771a809742653b5f9df5fa1e383ef
Rob Knop <raknop@lbl.gov>
2024-05-08 14:43:11 -0700

    runner for python multiprocessing

--- tests/util/test_runner.py
+++ tests/util/test_runner.py
@@ -1,3 +1,4 @@
+import multiprocessing
 from util.runner import Runner
 
 
@@ -9,20 +10,24 @@ class twoxplusone:
     def calc( self, x ):
         return 2 * x + 1
 
-    def summer( self, x ):
+    def summer( self, dex, x ):
         self.vals.append( x )
         self.tot += x
 
 class sometimesfails:
     def __init__( self ):
-        self.vals = []
+        self.args = [ True, True, False, True, False, False, True ]
+        self.args = [ ( i, self.args[i] ) for i in range(len(self.args)) ]
+        self.vals = [ -1 for i in range(len(self.args)) ]
 
-    def do( self, fail ):
-        if fail:
+    def do( self, arg ):
+        i, notfail = arg
+        if not notfail:
             raise RuntimeError( "Fail" )
+        return i
 
-    def aggregator( self, x ):
-        self.vals.append( x )
+    def aggregator( self, dex, x ):
+        self.vals[dex] = x
 
 
 def test_runner():
@@ -32,56 +37,52 @@ def test_runner():
         # Test when the number of values is < the number of processes
 
         doer = twoxplusone()
-        runner = Runner( numprocs,
-                         lambda x: doer.calc(x),
-                         aggregator=lambda x: doer.summer(x),
-                         persistent_processes=persist )
+        runner = Runner( numprocs, doer.calc, aggregator=doer.summer, persistent_processes=persist )
         nargs, nsuccess, nfail = runner.go( [1, 2, 3] )
+        kids = multiprocessing.active_children()
+        assert all( [ not k.is_alive() for k in kids ] )
         assert nargs == 3
-        assert nsucces == 3
+        assert nsuccess == 3
         assert nfail == 0
-        assert doer.vals == [ 3, 5, 7 ]
+        assert set( doer.vals ) == { 3, 5, 7 }
         assert doer.tot == 15
 
         # Test when the number of vals is > the number of processses
 
         doer = twoxplusone()
-        runner = Runner( numprocs,
-                         lambda x: doer.calc(x),
-                         aggregator=lambda x: doer.summer(x),
-                         persistent_processes=persist )
+        runner = Runner( numprocs, doer.calc, aggregator=doer.summer, persistent_processes=persist )
         nargs, nsuccess, nfail = runner.go( [1, 2, 3, 4, 5, 6, 7] )
+        kids = multiprocessing.active_children()
+        assert all( [ not k.is_alive() for k in kids ] )
         assert nargs == 7
         assert nsuccess == 7
         assert nfail == 0
-        assert doer.vals == [ 3, 5, 7, 9, 11, 13, 15 ]
+        assert set( doer.vals ) == { 3, 5, 7, 9, 11, 13, 15 }
         assert doer.tot == 63
 
         # Test when the number of vals is = the number of processes
 
         doer = twoxplusone()
-        runner = Runner( numprocs,
-                         lambda x: doer.calc(x),
-                         aggregator=lambda x: doer.summer(x),
-                         persistent_processes=persist )
+        runner = Runner( numprocs, doer.calc, aggregator=doer.summer, persistent_processes=persist )
         nargs, nsuccess, nfail = runner.go( [1, 2, 3, 4] )
+        kids = multiprocessing.active_children()
+        assert all( [ not k.is_alive() for k in kids ] )
         assert nargs == 4
         assert nsuccess == 4
         assert nfail == 0
-        assert doer.vals == [ 3, 5, 7, 9 ]
+        assert set( doer.vals ) == { 3, 5, 7, 9 }
         assert doer.tot == 24
 
         # Test failures
 
         doer = sometimesfails()
-        runner = Runner( numprocs,
-                         lambda x: doer.do(x),
-                         aggregator=lambda x: doer.aggregator(x),
-                         persistent_processes=persist )
-        nargs, nsuccess, nfail = runner.go( [ True, True, False, True, False, False, True ] )
+        runner = Runner( numprocs, doer.do, aggregator=doer.aggregator, persistent_processes=persist )
+        nargs, nsuccess, nfail = runner.go( doer.args )
+        kids = multiprocessing.active_children()
+        assert all( [ not k.is_alive() for k in kids ] )
         assert nargs == 7
         assert nsuccess == 4
         assert nfail == 3
-        assert doer.vals == [ True, True, None, True, None, None, True ]
+        assert doer.vals == [ 0, 1, None, 3, None, None, 6 ]
         
 
--- util/runner.py
+++ util/runner.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import multiprocessing
 import logging
 
@@ -22,140 +23,156 @@ class Runner:
             formatter = logging.Formatter( f'[%(asctime)s - %(levelname)s] - %(message)s',
                                            datefmt='%Y-%m-%d %H:%M:%S' )
             logout.setFormatter( formatter )
-            self.logger.setLevel( logging.INFO )
+            # self.logger.setLevel( logging.INFO )
+            self.logger.setLevel( logging.DEBUG )
 
     def subproc( self, pipe, logger ):
         me = multiprocessing.current_process()
-        logger.info( "Process starting: {me.name} PID {me.pid}" )
+        self.logger.info( "Process starting: {me.name} PID {me.pid}" )
 
         try:
             done = False
             while not done:
                 command = pipe.recv()
                 if command['command'] == 'die':
-                    logger.debug( f"{me.name} : got die command" )
+                    self.logger.debug( f"{me.name} : got die command" )
                     done = True
 
                 elif command['command'] == 'do':
                     try:
                         if 'info' in command:
-                            logger.info( f'{me.name} running: {command["info"]}' )
+                            self.logger.info( f'{me.name} running: {command["info"]}' )
                         res = self.executor( command['arg'] )
                         pipe.send( { 'status': 'done', 'result': res } )
                     except Exception as ex:
                         s = f"{me.name} exception: {ex}"
-                        logger.Exception( s )
+                        self.logger.exception( s )
                         pipe.send( { 'status': 'error', 'error': s } )
 
                     if not self.persistproc:
                         done = True
 
         except Exception as ex:
-            logger.exception( f"Process {me.name} PID {me.pid} returning after exception" )
+            self.logger.exception( f"Process {me.name} PID {me.pid} returning after exception" )
             return
 
-        logger.debug( f"Process {me.name} PID {me.pid} " )
+        self.logger.debug( f"Process {me.name} PID {me.pid} exiting " )
         return
 
+
+    def makeproc( self, name ):
+        mypipe, theirpipe = multiprocessing.Pipe( True )
+        proc = multiprocessing.Process( target=lambda: self.subproc( theirpipe, self.logger ), name=name )
+        proc.start()
+        self.allprocs.append( proc )
+        return proc, mypipe
+
+    def poll_busy( self ):
+        busydex = 0
+        while busydex < len( self.busyprocs ):
+            pipe = self.busypipes[ busydex ]
+            if pipe.poll():
+                proc = self.busyprocs[ busydex ]
+                argdex = self.busyargdex[ busydex ]
+                self.logger.debug( f"Getting response from {proc.name}" )
+                rval = pipe.recv()
+                fail = False
+                if 'status' not in rval:
+                    self.logger.error( f"Unexpected response from {proc.name}: {rval}" )
+                    self.nfail += 1
+                    fail = True
+                elif rval['status'] == 'error':
+                    self.logger.error( f"Error return from {proc.name}: {rval}" )
+                    self.nfail += 1
+                    fail = True
+                else:
+                    self.nsuccess += 1
+                if self.aggregator is not None:
+                    self.aggregator( argdex, None if fail else rval['result'] )
+                if self.persistproc:
+                    self.idleprocs.append( self.busyprocs[ busydex ] )
+                    self.idlepipes.append( self.busypipes[ busydex ] )
+
+                del self.busyprocs[ busydex ]
+                del self.busypipes[ busydex ]
+                del self.busyargdex[ busydex ]
+            else:
+                busydex += 1
+    
     def go( self, args ):
 
-        idleprocs = []
-        idlepipes = []
-        busyprocs = []
-        busypipes = []
-        nargs = len(args)
-        nsent = 0
-        nsuccess = 0
-        nfail = 0
+        self.idleprocs = []
+        self.idlepipes = []
+        self.busyprocs = []
+        self.busypipes = []
+        self.busyargdex = []
+        self.allprocs = []
+        self.nargs = len(args)
+        self.nsent = 0
+        self.nsuccess = 0
+        self.nfail = 0
 
-        def makeproc( i ):
-            mypipe, theirpipe = multiprocessing.Pipe( True )
-            proc = multiprocessing.Process( target=lambda: self.subprocs( theirpipe, self.logger ),
-                                            name=f"proc {i}" )
-            proc.start()
-            return proc, mypipe
-
-        def collect( proc, pipe ):
-            logger.debug( f"Getting response from {proc.name}" )
-            rval = pipe.recv()
-            fail = False
-            if 'status' not in rval:
-                logger.error( f"Unexpected response from {proc.name}: {rval}" )
-                nfail += 1
-                fail = True
-            elif rval['status'] == 'error':
-                logger.error( f"Error return from {proc.name}: {rval}" )
-                nfail += 1
-                fail = True
-            else:
-                nsuccess += 1
-            if self.aggregator is not None:
-                self.aggregator( None if fail else rval )
-            if self.persistproc:
-                idleprocs.append( busyprocs[busydex] )
-                idlepipes.append( busypipes[busydex] )
-                
-            
         if self.persistproc:
-            self.loggerr.info( f"Creating {self.numprocs} persistent worker processes"  )
-            for i in range(numprocs):
-                proc, pipe = makeproc( i )
-                idleprocs.append( proc )
-                idlepipes.append( pipe )
+            self.logger.info( f"Creating {self.numprocs} persistent worker processes"  )
+            for i in range( self.numprocs ):
+                proc, pipe = self.makeproc( f"proc {i}" )
+                self.idleprocs.append( proc )
+                self.idlepipes.append( pipe )
 
-        logger.info( f"Beinning to work through {len(args)} jobs" )
+        self.logger.info( f"Beinning to work through {len(args)} jobs" )
 
-        args = args.copy()
-        while len(args) > 0:
+        argdex = 0
+        while argdex < len(args):
 
             # Launch jobs until we have max processes running
             
-            while ( len(args) > 0 ) and ( len(busyprocs) < self.numprocs ):
+            while ( argdex < len(args) ) and ( len(self.busyprocs) < self.numprocs ):
                 proc = None
                 pipe = None
                 if self.persistproc:
-                    proc = idleprocs.pop()
-                    pipe = idlepipes.pop()
+                    proc = self.idleprocs.pop()
+                    pipe = self.idlepipes.pop()
                 else:
-                    proc, pipe = makeproc( nsent )
-                busyprocs.append( proc )
-                busypipes.append( pipe )
-                arg = args.pop()
-                logger.info( f"Sending job {nsent} / {nargs} to {proc.name}" )
-                pipe.send( arg )
+                    proc, pipe = self.makeproc( f"proc {self.nsent}" )
+                self.busyprocs.append( proc )
+                self.busypipes.append( pipe )
+                self.busyargdex.append( argdex )
+                self.logger.info( f"Sending job {self.nsent} / {self.nargs} to {proc.name}" )
+                pipe.send( { 'command': 'do', 'arg': args[argdex] } )
+                argdex += 1
+                self.nsent += 1
 
             # Collect results from any finished proces
 
-            busydex = 0
-            while busydex < len( busyprocs ):
-                if busypipes[busydex].poll():
-                    collect( busyprocs[busydex], busypipes[busydex] )
-                    del busyprocs[ busydex ]
-                    del busypipes[ busydex ]
-                else:
-                    busydex += 1
+            self.poll_busy()
 
-        logger.info( f"Have sent {sent} / {nargs} jobs, {nfail+nsuccess} are done ({nfail} failures), "
-                     f"waiting for {len(busyprocs)} processes to finish" )
+        self.logger.info( f"Have sent {self.nsent} / {self.nargs} jobs, "
+                          f"{self.nfail+self.nsuccess} are done ({self.nfail} failures), "
+                     f"waiting for {len(self.busyprocs)} processes to finish" )
 
-        while len(busyprocs) > 0:
-            busydex = 0
-            while busydex < len( busyprocs ):
-                if busypipes[busydex].poll():
-                    collect( busyprocs[busydex], busypipes[busydex] )
-                    del busyprocs[ busydex ]
-                    del busypipes[ busydex ]
-                else:
-                    busydex += 1
-            if ( len(busyprocs) > 0 ) and ( self.end_sleeptime > 0. ):
+        while len(self.busyprocs) > 0:
+            self.poll_busy()
+            if ( len(self.busyprocs) > 0 ) and ( self.end_sleeptime > 0. ):
                 time.sleep( self.end_sleeptime )
 
-        s = f"Runner all done; {nsuccess}/{nargs} successes, {nfail}/{nargs} failures."
+        s = f"Runner all done; {self.nsuccess}/{self.nargs} successes, {self.nfail}/{self.nargs} failures."
         if self.persistproc:
-            logger.info( f"{s}  Telling worker processes to die." )
-            for pipe in idlepipes:
+            self.logger.info( f"{s}  Telling worker processes to die." )
+            for pipe in self.idlepipes:
                 pipe.send( {'command': 'die'} )
         else:
-            logger.info( s )
+            self.logger.info( s )
 
-`        return nargs, nsuccess, nfail
+        # Join all subprocesses to make sure that they actually end.
+        # Since self.subproc doesn't have much to do after receiving
+        # a die command, I'm going to assume a timeout of 1 second
+        # is enough.  (Should be way more than enough.  The downside
+        # is, if there aren't persistent processes, and many hang,
+        # this will wait a long time.)
+
+        for proc in self.allprocs:
+            proc.join( 1 )
+
+        # TODO: Check to make sure all the processes are really finished, raise an exception if not
+            
+        return self.nargs, self.nsuccess, self.nfail
