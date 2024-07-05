b90dcd6bd2f688db101e1e83554816a5a47150b8
Rob Knop <raknop@lbl.gov>
2024-07-05 11:15:32 -0700

    Update test_image_querying for different decam images.  Patch DataStore merge_all to not have a problem saving sub image

--- pipeline/data_store.py
+++ pipeline/data_store.py
@@ -1497,9 +1497,9 @@ class DataStore:
             self.products_committed = 'image, sources, psf, wcs, zp, bg'
 
             if self.sub_image is not None:
-                import pdb; pdb.set_trace()
                 if self.reference is not None:
                     self.reference = self.reference.merge_all(session)
+                    self.sub_image.ref_image = self.reference.image
                 self.sub_image.new_image = self.image  # update with the now-merged image
                 self.sub_image = self.sub_image.merge_all(session)  # merges the upstream_images and downstream products
                 self.sub_image.ref_image.id = self.sub_image.ref_image_id
