4d6cf3abe0b56441f59f5d6c52686bae238bd387
Rob Knop (Nersc) <raknop@lbl.gov>
2024-08-30 06:51:17 -0700

    retrydownload would retry forever if md5sum mismatched

--- tests/util/test_retrydownload.py
+++ tests/util/test_retrydownload.py
@@ -65,6 +65,21 @@ def test_404():
     with pytest.raises( RuntimeError, match='3 exceptions trying to download.*, failing.' ):
         retry_download( url_nonexistent, f'{nonexistent}.dat', retries=3, sleeptime=1, exists_ok=False )
 
+def test_mismatch_md5():
+    fname = "".join( random.choices( string.ascii_lowercase, k=10 ) )
+    fpath = pathlib.Path( fname )
+    assert not fpath.exists()
+    try:
+        try:
+            retry_download( url1, fpath, exists_ok=False, md5sum="wrong" )
+        except Exception as e:
+            import pdb; pdb.set_trace()
+            pass
+    finally:
+        fpath.unlink( missing_ok=True )
+        assert not fpath.exists()
+
+
 def test_overwrite_misc_file():
     fname = "".join( random.choices( string.ascii_lowercase, k=10 ) )
     fpath = pathlib.Path( fname )
