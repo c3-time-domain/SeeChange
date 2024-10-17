2f323f05beb9e2e2b7d53ec3d8392a2cfeb72bcb
Rob Knop <raknop@lbl.gov>
2024-10-17 12:45:20 -0700

    Just starting on rbbot integration

--- pipeline/scoring.py
+++ pipeline/scoring.py
@@ -5,12 +5,16 @@ from pipeline.parameters import Parameters
 from pipeline.data_store import DataStore
 
 import numpy as np
+import torch
+
+import RBbot_inference
 
 from models.measurements import Measurements
 from models.deepscore import DeepScore
 from models.provenance import Provenance
 from models.enums_and_bitflags import DeepscoreAlgorithmConverter
 
+from util.config import Config
 from util.util import env_as_bool
 from util.logger import SCLogger
 
@@ -27,6 +31,14 @@ class ParsScorer(Parameters):
             'Valid names can be found in enums_and_bitflags.py.'
         )
 
+        self.rbbot_model_dir = self.add_par(
+            'rbbot_model_dir',
+            '/seechange/share/RBbot_models',
+            str,
+            'Directory where RBbot models may be found.',
+            critical=False
+        )
+
         self._enforce_no_new_attrs = True
 
         self.override(kwargs)
@@ -36,11 +48,28 @@ class ParsScorer(Parameters):
 
 class Scorer:
     def __init__(self, **kwargs):
-        self.pars = ParsScorer(**kwargs)
+        self.config = Config.get()
+
+        self.pars = ParsScorer( **(self.config.value('scoring')) )
+        self.pars.augment( kwargs )
 
         # this is useful for tests, where we can know if
         # the object did any work or just loaded from DB or datastore
         self.has_recalculated = False
+
+    def score_rbbot( self, ds, deepmodel ):
+        sources = ds.get_sources( session=session )
+        cutouts = ds.get_cutouts( session=session )
+        cutouts.load_all_co_data( sources=sources )
+
+        # Construct the numpy array
+        # Current RBbot models assume 41×41 cutouts
+
+        import pdb; pdb.set_trace()
+
+        RBbot_inference.load_model( deepmodel, model_root=self.pars.rbbot_model_dir )
+
+
 
     def run(self, *args, **kwargs):
         """
@@ -54,7 +83,7 @@ class Scorer:
             ds, session = DataStore.from_args(*args, **kwargs)
         except Exception as e:
             return DataStore.catch_failure_to_parse(e, *args)
-        
+
         try:
             t_start = time.perf_counter()
             if env_as_bool('SEECHANGE_TRACEMALLOC'):
@@ -73,45 +102,45 @@ class Scorer:
                     f'Cannot find a measurements corresponding to '
                     f'the datastore inputs: {ds.get_inputs()}'
                 )
-            
+
             # find if these deepscores have already been made
             scores = ds.get_scores( prov, session = session, reload=True )
 
             if scores is None or len(scores) == 0:
                 self.has_recalculated = True
+                algo = Provenance.get( d.provenance_id ).parameters['algorithm']
 
-                # go over each measurements object and produce a DeepScore object
-                scorelist = []
+                if ( algo == 'random' ) or ( algo =='allperfect' ):
+                    scorelist = []
+                    for m in measurements:
+                        d = DeepSCore.from_measurements( m, provenance=prov )
 
-                for m in measurements:
-                    d = DeepScore.from_measurements( m, provenance=prov )
+                        if algo == 'random':
+                            d.score = np.random.default_rng().random()
+                            d.algorithm = algo
 
-                    # Calculate the deepscore
+                        elif algo == 'allperfect':
+                            d.score = 1.0
+                            d.algorithm = algo
 
-                    algo = Provenance.get( d.provenance_id ).parameters['algorithm']
+                        # add it to the list
+                        scorelist.append( d )
 
-                    if algo == 'random':
-                        d.score = np.random.default_rng().random()
-                        d.algorithm = algo
+                elif algo[0:5] == 'RBbot':
+                    scorelist = self.score_rbbot( ds, algo )
 
-                    elif algo == 'allperfect':
-                        d.score = 1.0
-                        d.algorithm = algo
-                    
-                    elif algo in DeepscoreAlgorithmConverter.dict_inverse:
-                        raise NotImplementedError(f"algorithm {algo} isn't yet implemented")
-                    
-                    else:
-                        raise ValueError(f"{algo} is not a valid ML algorithm.")
+                elif algo in DeepscoreAlgorithmConverter.dict_inverse:
+                    raise NotImplementedError(f"algorithm {algo} isn't yet implemented")
 
-                    # add it to the list
-                    scorelist.append( d )
+                else:
+                    raise ValueError(f"{algo} is not a valid ML algorithm.")
+
 
                 scores = scorelist
 
             #   regardless of whether we loaded or calculated the scores, we need
             # to update the bitflag
-            
+
             # NOTE: zip only works since get_scores ensures score are sorted to measurements
             for score, measurement in zip( scores, measurements ):
                 score._upstream_bitflag = 0
